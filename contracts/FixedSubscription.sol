8642badee383f3c61a452412e9e7fb9ec7c2567f
Hithru <hithrudealwis@gmail.com>
2023-03-11 20:50:44 +0530

    Add mintToken for onlyOwner without payments

--- contracts/FixedSubscription.sol
+++ contracts/FixedSubscription.sol
@@ -57,6 +57,16 @@ contract FixedSubscriptionLicense is ERC721,Ownable{
         emit CreatedSubscriptionToken(s_tokenCounter, s_licensePrice);
     }
 
+    function mintToken(address customer) public onlyOwner {
+
+        transferingAllowed[s_tokenCounter] = 1;
+        _safeMint(customer, s_tokenCounter);
+
+        expirationTimestamp[s_tokenCounter] = block.timestamp.add(i_periodSecond);
+        transferingAllowed[s_tokenCounter] = 0;
+        s_tokenCounter = s_tokenCounter + 1;
+        emit CreatedSubscriptionToken(s_tokenCounter, s_licensePrice);
+    }
 
     function updateSubscription(uint256 tokenId) public payable {
         if (msg.value < s_licensePrice) {
--- contracts/PerpetualLicense.sol
+++ contracts/PerpetualLicense.sol
@@ -45,7 +45,12 @@ contract PerpetualLicense is ERC721Royalty,Ownable{
         emit CreatedLicenseToken(s_tokenCounter, s_licensePrice);
     }
 
+    function mintToken(address customer) public onlyOwner  {
 
+        _safeMint(customer, s_tokenCounter);
+        s_tokenCounter = s_tokenCounter + 1;
+        emit CreatedLicenseToken(s_tokenCounter, s_licensePrice);
+    }
 
     function _baseURI() internal pure override returns (string memory) {
         return "data:application/json;base64,";
