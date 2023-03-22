62743b375a7e0ce989de770628084fa39407bad2
Hithru <hithrudealwis@gmail.com>
2023-03-23 01:22:57 +0530

    Add license Activation Contract to use for activation

--- contracts/PerpetualLicense.sol
+++ contracts/PerpetualLicense.sol
@@ -4,34 +4,39 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Royalty.sol";
 import "base64-sol/base64.sol";
-
+import "./LicenseActivation.sol";
 
 error ERC721Metadata__URI_QueryFor_NonExistentToken();
 error PerpetualLicense__TransferFailed();
 error PerpetualLicense__NeedMoreETHSent();
+error PerpetualLicense__LicenseActivated();
 
-contract PerpetualLicense is ERC721Royalty,Ownable{
-
+contract PerpetualLicense is ERC721Royalty, Ownable {
     uint256 private s_tokenCounter;
     uint256 private s_licensePrice;
     string private i_companyName;
     string private i_licenseType;
     string private i_licenseName;
 
+    LicenseActivation private s_licenseActivationContract;
+
     event CreatedLicenseToken(uint256 indexed tokenId, uint256 licensePrice);
 
     constructor(
         string memory companyName,
         string memory licenseName,
-        uint256  licensePrice,
-        uint96 royaltyPrecentage
+        uint256 licensePrice,
+        uint96 royaltyPercentage,
+        address licenseActivationContractAddress
     ) ERC721("Software License", "SHK") {
         s_tokenCounter = 0;
         s_licensePrice = licensePrice;
         i_companyName = companyName;
         i_licenseName = licenseName;
         i_licenseType = "Perpetual";
-        _setDefaultRoyalty(msg.sender, royaltyPrecentage*100);
+        _setDefaultRoyalty(msg.sender, royaltyPercentage * 100);
+
+        s_licenseActivationContract = LicenseActivation(licenseActivationContractAddress);
     }
 
 
@@ -105,5 +110,14 @@ contract PerpetualLicense is ERC721Royalty,Ownable{
     function updateLicensePrice(uint256 newPrice) public onlyOwner {
         require(newPrice > 0, "Price must be greater than zero");
         s_licensePrice = newPrice;
+    }
+
+    function _transfer(address from, address to, uint256 tokenId) internal virtual override {
+        require(
+            !s_licenseActivationContract.isLicenseActivated(tokenId),
+            "Cannot transfer an activated license"
+        );
+
+        super._transfer(from, to, tokenId);
     }
 }
