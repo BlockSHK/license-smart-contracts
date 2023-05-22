fc1387bd22e29d485611e9c92dd279e46c92aba4
Hithru <hithrudealwis@gmail.com>
2023-05-22 12:51:40 +0530

    Update hardhat configuration to add sepolia network and new dummy accounts

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,10 +10,13 @@ const { solidityDocgen } = require("solidity-docgen")
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
 const GOERLI_RPC_URL =
     process.env.GOERLI_RPC_URL ||
-    "https://eth-goerli.g.alchemy.com/v2/wLpA-TL3WxjCfKMfTcaxJQ7fL6MIQ1mP"
-const PRIVATE_KEY =
-    process.env.PRIVATE_KEY ||
-    "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
+    "https://eth-goerli.g.alchemy.com/v2/[YOUR-API-KEY]"
+const SEPOLIA_RPC_URL =
+    process.env.SEPOLIA_RPC_URL ||
+    "https://eth-sepolia.g.alchemy.com/v2/[YOUR-API-KEY]"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "" //Account 10
+const PRIVATE_KEY_TWO = process.env.PRIVATE_KEY_TWO || "" //Account 9
+const PRIVATE_KEY_THREE = process.env.PRIVATE_KEY_THREE || "" //Account 1
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 
 module.exports = {
@@ -21,17 +24,19 @@ module.exports = {
     networks: {
         hardhat: {
             chainId: 31337,
-            // blockGasLimit: 60000000
-            // gasPrice: 130000000000,
         },
         goerli: {
             url: GOERLI_RPC_URL,
-            accounts: [
-                PRIVATE_KEY,
-                "5066cd290a3242f79203266229634d0c9a1ba328ca0d7c3c41343f0ff22dee76",
-            ],
+            accounts: [PRIVATE_KEY, PRIVATE_KEY_TWO, PRIVATE_KEY_THREE],
             chainId: 5,
             blockConfirmations: 6,
+        },
+
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY, PRIVATE_KEY_TWO, PRIVATE_KEY_THREE],
+            chainId: 11155111,
+            blockConfirmations: 3,
         },
     },
     solidity: {
@@ -68,10 +73,17 @@ module.exports = {
         deployer: {
             default: 0, // here this will by default take the first account as deployer
             1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+            2: 0,
         },
         secondPayer: {
             default: 1,
             1: 1,
+            2: 1,
+        },
+        thirdAccount: {
+            default: 2,
+            1: 2,
+            2: 2,
         },
     },
     mocha: {
